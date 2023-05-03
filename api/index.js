91ad52820f4915c3157212b4727481c907978f23
lceras <luisfcss1102@hotmail.com>
2023-05-03 18:55:50 -0500

    modificando puerto

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@
 require('dotenv').config();
 const server = require('./src/app.js'); 
 const { conn } = require('./src/db.js');      
-const PORT= process.env.PORT || 3001  
+const {PORT}= process.env  
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
