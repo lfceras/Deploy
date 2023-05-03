47e60b88ee1a34880a3fff872e959f8fa2afb918
lceras <luisfcss1102@hotmail.com>
2023-05-03 18:00:19 -0500

    restructuring data

--- api/config.js
+++
@@ -1,5 +0,0 @@
-import {config} from 'dotenv'
-
- config()
-
-export const PORT= process.env.PORT || 3001
--- api/index.js
+++ api/index.js
@@ -18,16 +18,13 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 require('dotenv').config();
-// const { PORT } = require('./config.js');
 const server = require('./src/app.js'); 
-const { conn } = require('./src/db.js');
-const {PORT} = process.env;      
-
-// const PORT =process.env.PORT || 3001
+const { conn } = require('./src/db.js');      
+const PORT= process.env.PORT || 3001  
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
-    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
+    console.log(`%s listening at ${PORT}`);
   });
 });
